fc0d27576adf23e5be59289ec38f9700415d296e
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-11 22:37:50 -0300

    Exercício realizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string fraseOriginal;
+
+Console.Write("Por favor, digite algo que tenha a letra R: ");
+fraseOriginal = Console.ReadLine()!; // Armazena o conteúdo digitado pelon usuário na variável
+
+Console.WriteLine(fraseOriginal.Replace("r", "l").Replace("R", "L")); // Exibe novamente a frase, substituindo r e R com l e L
